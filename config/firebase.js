4a2a5b7cba5911740c80e79c09cd9c3fa1e40914
harsh <harsh@harshs-MacBook-Air.local>
2023-09-08 22:23:20 +0530

    Integration with Firebase

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
+// Add Firebase SDK Snippet
+  // databaseURL: "",
+  // measurementId: "",
+const firebaseConfig = {
+  apiKey: "AIzaSyAPpjED3wRoJQ1_RHKHu9rEth7kLPWWTLA",
+  authDomain: "whms-auth-7ed4b.firebaseapp.com",
+  projectId: "whms-auth-7ed4b",
+  storageBucket: "whms-auth-7ed4b.appspot.com",
+  messagingSenderId: "198515750686",
+  appId: "1:198515750686:web:69b3755dea12a23010fb0f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+module.exports = firebase;
