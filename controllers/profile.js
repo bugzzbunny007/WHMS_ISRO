bd070870b82d03f0b608587800e967f4c37ffdd1
Shivanshu Bhargava <shivanshu.bhargava@capgemini.com>
2023-10-16 02:02:20 +0530

    updated get-profile route

--- controllers/profile.js
+++ controllers/profile.js
@@ -41,22 +41,22 @@ exports.getProfile = async (req, res) => {
     console.log(req.user)
     // Use the upsert option to either update or insert the profile
 
-    Profile.updateOne(
-        { _id: authId }, // Find the profile with the specified _id
-        { age, weight, gender, height }, // Update the profile with the new data
-        { upsert: true } // Create a new profile if it doesn't exist
-    ).then((User) => {
-        console.log(User);
-        return res.status(200).json({ message: "Profile updated" })
-    }).catch(function (error) {
-        // let errorCode = error.code;
-        let errorMessage = error.message;
-        logger.logToCloudWatch(formattedDate.toString(), `${errorMessage}`);
+    // Profile.updateOne(
+    //     { _id: authId }, // Find the profile with the specified _id
+    //     { age, weight, gender, height }, // Update the profile with the new data
+    //     { upsert: true } // Create a new profile if it doesn't exist
+    // ).then((User) => {
+    //     console.log(User);
+    //     return res.status(200).json({ message: "Profile updated" })
+    // }).catch(function (error) {
+    //     // let errorCode = error.code;
+    //     let errorMessage = error.message;
+    //     logger.logToCloudWatch(formattedDate.toString(), `${errorMessage}`);
 
-        console.log(errorMessage)
+    //     console.log(errorMessage)
 
-        //     console.log(errorMessage)
-    })
+    //     //     console.log(errorMessage)
+    // })
     const ProfileData = await Profile.findOne({ _id: req.user.user_id }).then((data) => {
         if (data) {
             return res.status(200).json(data)
