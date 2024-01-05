a8e5ec99b965d5cf220cadaa23c95096e8942f50
piyushjagtap22 <jagspiyush@gmail.com>
2024-01-05 13:34:51 +0530

    Added expired token case for fetchuser middleware

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -12,6 +12,7 @@ const fetchUser = (req, res, next) => {
     // console.log(req.get('Authorization'))
     // Get the user's ID token from the request headers or wherever it's stored
     const idToken = req.get('Authorization');
+    console.log("In middleware")
     // console.log(idToken)
     if (!idToken) {
         return res.status(401).json({ error: "Unauthorized" });
@@ -21,14 +22,21 @@ const fetchUser = (req, res, next) => {
         .auth()
         .verifyIdToken(idToken.split(" ")[1])
         .then((decodedToken) => {
+            console.log("Here 25")
             // Add the user object to the request for use in controllers
             req.user = decodedToken;
             next();
         })
         .catch((error) => {
-            console.error(error)
-            logger.logToCloudWatch(formattedDate.toString(), `Issue in fetchuser: ${error}`);
-            return res.status(500).json({ error: "Internal Server Error" });
+            console.log(error.code)
+            if (error.code == "auth/id-token-expired") {
+                logger.logToCloudWatch(formattedDate.toString(), `Issue in fetchuser: ${error}`);
+                return res.status(401).json({ error: "Token Expired" });
+            }
+            else {
+                logger.logToCloudWatch(formattedDate.toString(), `Issue in fetchuser: ${error}`);
+                return res.status(500).json({ error: "Internal Server Error" });
+            }
         });
 };
 
