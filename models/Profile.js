3e1add4cdc7c2d040eba0e4f9a3c14c05bbc2f96
piyushjagtap22 <jagspiyush@gmail.com>
2024-01-05 12:15:26 +0530

    age to dob

--- models/Profile.js
+++ models/Profile.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 // Mongoose Schema for User
 const ProfileSchema = new Schema({
     _id: { type: String, required: true },
-    age: { type: Number },
+    dob: { type: String },//
     weight: { type: Number },
     height: { type: Number },
     gender: { type: String },
