3314435085ef3a1881fd2afc2aec836ab9b689ef
piyushjagtap22 <jagspiyush@gmail.com>
2024-01-15 21:39:51 +0530

    minor update

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI_TEST, {
+        await mongoose.connect(process.env.MONGO_URI, {
         });
         console.log("Connected to MongoDb")
     } catch (err) {
