b80abe11981e525be8a2a61c69b58b0bcca4ff28
piyushjagtap22 <jagspiyush@gmail.com>
2024-09-03 23:49:15 +0530

    Mail text updated

--- utils/mail.js
+++ utils/mail.js
@@ -57,14 +57,14 @@ const generateMail = (name, email, subject, content) => {
             <div style="font-family: Helvetica, Arial, sans-serif; min-width: 1000px; overflow: auto; line-height: 2">
                 <div style="margin: 50px auto; width: 70%; padding: 20px 0">
                     <div style="border-bottom: 1px solid #eee">
-                        <a href="" style="font-size: 1.4em; color: #00466a; text-decoration: none; font-weight: 600">WHMS</a>
+                        <a href="" style="font-size: 1.4em; color: #00466a; text-decoration: none; font-weight: 600">W-HMS</a>
                     </div>
                     <p style="font-size: 1.1em">Hi ${name},</p>
                     ${content}
-                    <p style="font-size: 0.9em;">Regards,<br />WHMS</p>
+                    <p style="font-size: 0.9em;">Regards,<br />W-HMS</p>
                     <hr style="border: none; border-top: 1px solid #eee" />
                     <div style="float: right; padding: 8px 0; color: #aaa; font-size: 0.8em; line-height: 1; font-weight: 300">
-                        <p>WHMS Inc</p>
+                        <p>W-HMS Inc</p>
                         <p>12 Cross Street, Bengaluru</p>
                     </div>
                 </div>
